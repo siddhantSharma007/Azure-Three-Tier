2afb738a3a912752548663051237c580d80ca833
siddhant sharma <siddhantsharmaaligarh@gmail.com>
2024-06-06 16:28:36 +0530

    first commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'mynewmysqlserver.mysql.database.azure.com',
+    DB_USER : 'adminuser',
+    DB_PWD : 'Priya@568',
+    DB_DATABASE : 'webappdb'
+});
